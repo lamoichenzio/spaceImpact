45c4a631ea00151bc5393151c05f12ca7edc0575
Alessandro D'Errico <dott.derricoalessandro@gmail.com>
2020-02-15 20:12:04 +0100

    upload project

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import { Shuttle, Fire } from "./shuttle";
+
+var row = 30
+var column = 100
+
+var shuttle;
+
+let createWorld = function() {
+    var table = document.getElementById("world");
+    for (var i = 1; i <= row; i++) {
+        var tr = document.createElement("tr");
+        for (var e = 1; e <= column; e++) {
+            var td = document.createElement("td");
+            td.id = i + "-" + e;
+            td.title = "cell"
+            td.className = "cell";
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+}
+
+function checkKey(e) {
+    e = e || window.event;
+    if (e.keyCode == '38') {
+        shuttle.moveUp();
+    } else if (e.keyCode == '40') {
+        shuttle.moveDown();
+    }
+    // } else if (e.keyCode == '37') {
+    //     shiftL();
+    // } else if (e.keyCode == '39') {
+    //     shiftR();
+    // }
+
+}
+
+window.onload = function() {
+
+    shuttle = new Shuttle();
+
+    this.console.log(shuttle);
+
+    document.getElementById("startGame").addEventListener("click", () => {
+        shuttle.createShuttle();
+        let fire = new Fire(shuttle);
+        setInterval(function() { fire.moveFire(); }, 10);
+
+    });
+    document.getElementById("createButton").addEventListener("click", createWorld);
+
+    document.addEventListener("keydown", (e) => checkKey(e));
+
+
+
+}
---
+++ src/shuttle.js
@@ -0,0 +1,121 @@
+class Shuttle {
+    constructor() {
+        this.color = "#000";
+        this.x = [2, 2, 2, 3];
+        this.y = [14, 15, 16, 15];
+        this.id = "shuttle";
+        this.className = "shuttle";
+        this.title = "shuttle";
+    }
+
+    get() {
+        return this;
+    }
+
+    createShuttle() {
+        let positions = [];
+        for (let i = 0; i <= 3; i++) {
+            positions.push(this.y[i] + "-" + this.x[i]);
+        }
+        for (let position of positions) {
+            var cell = document.getElementById(position);
+            cell.className = this.className;
+        }
+    }
+
+    moveUp() {
+
+        if (this.y[0] > 2) {
+
+            let oldPositions = [];
+            let newPosition = [];
+            for (let i = 0; i <= 3; i++) {
+                oldPositions.push(this.y[i] + "-" + this.x[i]);
+                this.y[i] -= 2;
+                newPosition.push(this.y[i] + "-" + this.x[i]);
+            }
+
+            for (let position of oldPositions) {
+                let cell = document.getElementById(position);
+                cell.className = "cell";
+                cell.title = "cell";
+            }
+
+            for (let position of newPosition) {
+                let cell = document.getElementById(position);
+                cell.className = this.className;
+                cell.title = this.title;
+            }
+        }
+    }
+
+    moveDown() {
+        if (this.y[2] < 29) {
+
+            let oldPositions = [];
+            let newPosition = [];
+            for (let i = 0; i <= 3; i++) {
+                oldPositions.push(this.y[i] + "-" + this.x[i]);
+                this.y[i] += 2
+                newPosition.push(this.y[i] + "-" + this.x[i]);
+            }
+
+            for (let position of oldPositions) {
+                let cell = document.getElementById(position);
+                cell.className = "cell";
+                cell.id = position;
+            }
+            for (let position of newPosition) {
+                let cell = document.getElementById(position);
+                cell.className = this.className;
+                // cell.id = this.id;
+            }
+        }
+    }
+
+}
+
+class Fire {
+    constructor(shuttle) {
+        let nave = shuttle.get();
+        this.x = nave.x[3] + 1;
+        this.y = nave.y[1];
+        this.className = "fire";
+        this.title = "fire";
+    }
+
+    get() {
+        return this;
+    }
+
+    moveFire() {
+        console.log("SHOOTING");
+
+        var cell = document.getElementById(this.y - 1 + "-" + this.x);
+        console.log(cell);
+
+        if (cell.id != "shuttle") {
+            cell.className = "cell";
+            cell.title = "cell";
+        }
+
+        var cell = document.getElementById(this.y + "-" + this.x);
+
+        console.log(cell);
+        cell.className = this.className;
+        cell.title = this.title;
+
+        this.x += 1;
+
+        console.log(this);
+    }
+
+    // shoot() {
+
+    // }
+
+
+
+}
+
+export { Shuttle, Fire };
